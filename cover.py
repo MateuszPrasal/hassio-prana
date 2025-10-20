a1e9cb3ceae267469dfbc6bfec2ad839ec6c9d62
MateuszPrasal <mateusz.prasal@gmail.com>
2025-10-20 14:55:24 +0200

    Implemented sensor

--- cover.py
+++ cover.py
@@ -19,8 +19,6 @@ from .coordinator import PranaCoordinator
 
 LOGGER = logging.getLogger(__name__)
 
-MAX_BRIGHTNESS = 6
-
 async def async_setup_entry(hass, config_entry, async_add_entities):
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
     async_add_entities([
